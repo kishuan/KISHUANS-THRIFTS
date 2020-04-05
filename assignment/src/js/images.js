bca399e3f3c8c507f1341ccbd28c60f5d666ef21
kishuan <59578767+kishuan@users.noreply.github.com>
2020-04-05 02:28:05 -0400

    Add files via upload

---
+++ assignment/src/js/images.js
@@ -0,0 +1,14 @@
+// images.js
+
+const products = [
+    {
+        accessories = ["images\accessories\f_b_buckethat.jpg", "images\accessories\f_champion_2.jpg", "images\accessories\f_champion_3.jpg", "images\accessories\f_champion_4.jpg", "images\accessories\f_champion_5.jpg","images\accessories\fanny_pack_1.jpg"]
+    },
+    {
+        feminine = ["images\fem\f_b_buckethat_2.jpg", "images\fem\f_b_cardigan_2.jpg", "images\fem\f_b_cardigan_3.jpg", "images\fem\f_b_cardigan.jpg", "images\fem\flared_pants_2.jpg", 
+    "images\fem\flared_pants.jpg", "images\fem\long_coat_2.jpg", "images\fem\long_coat.jpg", "images\fem\mock_neck_2.png", "images\fem\mock_neck.png", "images\fem\purple_cardigan.jpg", "images\fem\tanktop.jpg", "images\fem\wrap_2.jpg", "images\fem\wrap_3.jpg", "images\fem\wrap.jpg"]
+    },
+    {
+        masculine = ["images\masc\adidas_hoodie.gif", "images\masc\floral.gif", "images\masc\tactical_vest.jpg", "images\masc\turqouise_jacket.jpg"]
+    }
+];
